45631f5250a60a5809025f78efb1741f515db309
essential-robot-4364 <brandon.james414@gmail.com>
2019-10-27 17:16:31 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def my_all?(collection)
   b = 0
-  the_choosen = []
+  block_return_values = []
   while b < collection.length 
     the_choosen << yield(collection[b])
     b = b + 1 
